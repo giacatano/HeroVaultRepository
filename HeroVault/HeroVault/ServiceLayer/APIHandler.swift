67d878b67c2f09bc28236b8bbc0fbce00aa9cf39
giacatano <142597978+giacatano@users.noreply.github.com>
2024-04-02 08:43:38 +0200

    First feature added
    Created a feature called SearchCharacters

---
+++ HeroVault/HeroVault/ServiceLayer/APIHandler.swift
@@ -0,0 +1,74 @@
+//
+//  APIHandler.swift
+//  HeroVault
+//
+//  Created by Gia Catano on 2024/04/01.
+//
+
+import Foundation
+import CommonCrypto
+
+enum NetworkingRequestType: String {
+    case GET, POST, PATCH, PUT, DELETE
+}
+
+enum NetworkingError: Error {
+    case internalError
+    case invalidUrl
+    case invalidData
+    case parsingError
+}
+
+class APIHandler {
+    
+    func request<T: Codable>(path: String,
+                             networkType: NetworkingRequestType,
+                             model: T.Type,
+                             completion: @escaping (Result<T, NetworkingError>) -> Void) {
+        let endPoint = buildEndpoint(path: path)
+        guard let url = URL(string: endPoint) else {
+            completion(.failure(.invalidUrl))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = networkType.rawValue
+        request.allHTTPHeaderFields = ["Content-Type": "application/json"]
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard error == nil else {
+                completion(.failure(.internalError))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            do {
+                let object = try JSONDecoder().decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(object))
+                }
+            } catch {
+                completion(.failure(.parsingError))
+            }
+        }
+        dataTask.resume()
+    }
+    
+    private func buildEndpoint(path: String) -> String {
+        let timeStamp = Date().timeIntervalSince1970
+        let authorizationKey = "\(timeStamp)\(Constants.APIKeys.privateAPIKey)\(Constants.APIKeys.publicAPIKey)"
+        let hashString = md5HashedString(securedString: authorizationKey)
+        return "\(path)?&apikey=\(Constants.APIKeys.publicAPIKey)&ts=\(String(timeStamp))&hash=\(hashString)"
+    }
+    
+    private func md5HashedString(securedString: String) -> String {
+        let data = Data(securedString.utf8)
+        var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+        _ = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) in
+            CC_MD5(bytes.baseAddress, CC_LONG(data.count), &hash)
+        }
+        return hash.map { String(format: "%02x", $0) }.joined()
+    }
+}
---
+++ HeroVault/HeroVault/ServiceLayer/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  HeroVault
+//
+//  Created by Gia Catano on 2024/03/30.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct EndPoints {
+        static let marvelCharacterNames =  "https://gateway.marvel.com:443/v1/public/characters"
+        
+        //add more endpoints here
+    }
+
+    struct APIKeys {
+        static let privateAPIKey = "73a28de7a8768594dd2e68edda1d36370c08290b"
+        static let publicAPIKey = "8b210265935b59224adf09a480932131"
+    }
+}
