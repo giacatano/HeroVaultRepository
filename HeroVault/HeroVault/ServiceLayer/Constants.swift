b52a0b04f4fe77ea13eae627371ed0297df699c0
giacatano <142597978+giacatano@users.noreply.github.com>
2024-04-03 13:14:14 +0200

    Third commit

--- HeroVault/HeroVault/ServiceLayer/Constants.swift
+++ HeroVault/HeroVault/ServiceLayer/Constants.swift
@@ -12,10 +12,10 @@ struct Constants {
     struct EndPoints {
         static let marvelCharacterNames =  "https://gateway.marvel.com:443/v1/public/characters"
         static let marvelComicTitles =
-        "https://gateway.marvel.com:443/v1/public/comics"
-        //add more endpoints here
+            "https://gateway.marvel.com:443/v1/public/comics"
+        static let marvelStoryDescriptions = "https://gateway.marvel.com:443/v1/public/events"
     }
-
+    
     struct APIKeys {
         static let privateAPIKey = "73a28de7a8768594dd2e68edda1d36370c08290b"
         static let publicAPIKey = "8b210265935b59224adf09a480932131"
