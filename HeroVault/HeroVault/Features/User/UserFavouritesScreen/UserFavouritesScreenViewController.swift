35fd5cc8f5d5d0959553e2fb05f300a64e627e80
giacatano <142597978+giacatano@users.noreply.github.com>
2024-04-30 21:41:52 +0200

    Edited Collection View

--- HeroVault/HeroVault/Features/User/UserFavouritesScreen/UserFavouritesScreenViewController.swift
+++ HeroVault/HeroVault/Features/User/UserFavouritesScreen/UserFavouritesScreenViewController.swift
@@ -11,6 +11,7 @@ class UserFavouritesScreenViewController: UIViewController, ViewModelDelegate {
     
     // MARK: IBOutlets
     
+    @IBOutlet weak var starImageView: UIImageView!
     @IBOutlet weak private var profileLabel: UILabel!
     @IBOutlet weak private var userImage: UIImageView!
     @IBOutlet weak private var favouritesTitleLabel: UILabel!
